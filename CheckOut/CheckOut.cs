a3953a852d951858cbc7eb66e8ad2e396074c973
BermarTech <paul.william.sheehan@gmail.com>
2017-01-28 13:32:09 +0000

    refactor with Sku as entity

--- CheckOut/CheckOut.cs
+++ CheckOut/CheckOut.cs
@@ -8,34 +8,38 @@ namespace CheckOut
 {
     public class CheckOut : ICheckOut
     {
-        private int total = 0;
+        private List<Sku> skus = new List<Sku>();
 
         public void Scan(string item)
         {
+            var sku = new Sku(item);
+
             if (item == "A")
             {
-                total += 50;
+                sku.UnitPice = 50;
             }
 
             if (item == "B")
             {
-                total += 30;
+                sku.UnitPice = 30;
             }
 
             if (item == "C")
             {
-                total += 20;
+                sku.UnitPice = 20;
             }
 
             if (item == "D")
             {
-                total += 15;
+                sku.UnitPice = 15;
             }
+
+            skus.Add(sku);
         }
 
         public int GetTotalPrice()
         {
-            return total;
+            return skus.Sum(x => x.UnitPice);
         }
     }
 }
---
+++ CheckOut/Sku.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CheckOut
+{
+    public class Sku
+    {
+        private string SkuType;
+
+        public int UnitPice;
+
+        public Sku(string skuType)
+        {
+            SkuType = skuType;
+        }
+    }
+}
